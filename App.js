1463daa29292aecd0b3c27faca926163fe87506a
raj1947 <rajkumar.lebara@gmail.com>
2025-02-19 13:06:21 +0530

    Before Actual project starts

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
-import React  from "react";
-import ReactDOM from "react-dom";
-const heading = React.createElement("h1", { id: "heading" }, "Hello world from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// JSX is HTML like syntax not HTML
+
+const jsxHeading = <h1 id="heading">Hello world using JSX!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+const TitleComponent = () => {
+  return <h1 id="heading">Hello world using React component!</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <TitleComponent />
+    </div>
+  );
+};
+
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Notes
 
 ...
+# Bable convert JSX to React element
+# JSX
+ - () is used in case we have multiple lines
+ - class renamed to className and JSX uses camelCase for naming
+ - It sanitize the data and prevent Cross site scripting
+# Component
+ - Componen composition - Component inside component
+ - Class base component - OLD
+ - Functional base component 
+    - NEW 
+    - Function return a JSX code
+    - Starts with Upper case
 # Parcel
 - HMR - Hot module replacement (Update browser on page save)
 - Dev Build
